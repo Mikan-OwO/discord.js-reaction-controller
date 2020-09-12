378e695052873528b3422108fba5dcc4ddd1caf2
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-09-13 05:58:20 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+exports.ReactionController = class ReactionController {
+  constructor(options = {}) {
+    this.client = options.client;
+  }
+  send(message) {
+    this.message = message;
+  }
+  addPage(embed) {
+    
+  }
+}
